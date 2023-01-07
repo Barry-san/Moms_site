56de6bbaeca47f77365c533f001c57b179214e76
Barry-san <oyeyemimubarak6@gmail.com>
2023-01-07 12:49:44 +0100

    made colors uniform

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,9 +1,15 @@
 function Navbar() {
-    return (  
-        <div className="flex justify-center align-center text-red-300 font-Oswald">
-            <p className="tracking-widest sticky top-0 text-xl md:text-3xl lg:text-4xl font-bold py-3" > G  I  A</p>
-        </div>
-    );
+  return (
+    <div className="flex sticky top-0 p-8 items-center justify-between text-center text-blue-500 font-Oswald">
+      <p className="tracking-widest sticky top-0 text-xl md:text-3xl lg:text-4xl font-bold py-3">
+        {" "}
+        G I A
+      </p>
+      {/* <a href="" className="text-lg md:text-2xl">
+        Address
+      </a> */}
+    </div>
+  );
 }
 
 export default Navbar;
