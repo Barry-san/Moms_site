a5f57d6fcebf7b9cd1033a96dfdcd92c8273cc7e
Barry-san <oyeyemimubarak6@gmail.com>
2023-01-07 13:45:10 +0100

    added styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,11 @@ module.exports = {
     extend: {
       fontFamily: {
         Oswald: ["'Oswald'", "sans-serif"],
+        Barlow: ["'Barlow'", "sans-serif"],
+      },
+      backgroundImage: {
+        pepsi: "url('../public/nikhil-82LJQZGwW5o-unsplash.jpg')",
+        coke: "url('../public/mae-mu-z8PEoNIlGlg-unsplash.jpg')",
       },
     },
   },
